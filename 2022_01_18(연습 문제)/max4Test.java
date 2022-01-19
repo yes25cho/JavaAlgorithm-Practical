09a0c2fe1e4c914b4dc6297dca486c69f12d65bf
yes25 <83991028+yes25cho@users.noreply.github.com>
2022-01-19 12:18:05 +0900

    Q1.네 값의 최뎃값을 구하는 max4 메서드를 작성하세요.
    max4 메소드 형식 
    static int max4(int a, int b, int c, int d)

---
+++ 2022_01_18(연습 문제)/max4Test.java
@@ -0,0 +1,32 @@
+package chap01;
+import java.util.Scanner;
+//Q1.네 값의 최뎃값을 구하는 max4 메서드를 작성하세요.
+//max4 메소드
+//static int max4(int a, int b, int c, int d)
+public class max4Test {
+	static int max4(int a, int b, int c, int d) {
+		int max = a;
+		if(max<b)
+			max = b;
+		if(max<c)
+			max = c;
+		if(max<d)
+			max = d;
+		
+		return max; 
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("네 값의 최뎃값을 구하는 프로그램");
+		System.out.print("a의 값 입력 : "); int a= sc.nextInt();
+		System.out.print("b의 값 입력 : "); int b= sc.nextInt();
+		System.out.print("c의 값 입력 : "); int c= sc.nextInt();
+		System.out.print("d의 값 입력 : "); int d= sc.nextInt();
+		
+		System.out.println();
+		System.out.println("최댓값은 "+max4(a, b, c, d)+"입니다.");
+		sc.close();
+	}
+
+}
